5ba42939f904270739fe32a3235f31f7f4b3015a
kubaa1501 <kubaa1501@interia.pl>
2024-05-14 14:47:51 +0200

    final code

--- 3_in_1_code_with_trained_classifier.py
+++ 3_in_1_code_with_trained_classifier.py
@@ -1,49 +1,3 @@
-# %% [markdown]   
-# # 3 in one code, with trained classifier.
-# ### INPUT:
-#     - images folder path
-#     - masks foler path
-#     - path for the output file .csv with results of all 3 featues and the probability of a lesion to be melanoma
-# ### OUTPUT:
-#     - .csv with results of all 3 featues and the probability of a lesion to be melanoma  
-# 
-# #### Output understanding:
-# ##### image_path     
-# ##### color_score   
-#               ( 1 - relatively dull or monochromatic,  
-#                 2 - some color variation, but it may lack intensity or diversity,   
-#                 3 - significant color variation with vibrant and diverse hues,   
-#                 4 - intense and varied colors, resembling a rainbow )  
-#                  
-# ##### symmetry_score 
-#                ( 1 - none symmetry      
-#                  2 - low symmetry        
-#                  3 - moderate symmetry       
-#                  4 - perfect symmetry )         
-# ##### blue_white_score 
-#                  ( 0 - blue-white veil not detected,
-#                    1 - blue-white veil detected)  
-#   
-# ##### probability_0 
-#     [0;1] confidance that the lesion belongs to class 0 (non melanoma)  
-# ##### probability_1  
-#     [0;1] confidance that the lesion belongs to class 1 (melanoma)  
-#   
-# Example, if probability_0 is 0.7 and probability_1 is 0.3 for a particular image-mask pair, it means that the classifier is 70% confident that the pair belongs to class 0 and 30% confident that it belongs to class 1.
-#     
-#     
-#         
-# The classifier is: "trained_random_forest_classifier.pkl" on the main page of the repository.
-# Please input the paths with " / " not " \ "   
-#   
-#     
-# #### example of inputs:  
-#      - image_folder = "C:/Users/cieci/OneDrive/Dokumenty/GitHub/G/images_orginal"  
-#      - mask_folder = "C:/Users/cieci/OneDrive/Dokumenty/GitHub/G/masks_orginal"  
-#      - output_csv = "C:/Users/cieci/OneDrive/Dokumenty/GitHub/G/results.csv"  
-# 
-
-# %%
 from PIL import Image
 import cv2
 import numpy as np
@@ -331,6 +285,3 @@ if __name__ == "__main__":
     # process images
     process_images(image_folder, mask_folder, output_csv, classifier)
     print("done:)")
-
-
-
